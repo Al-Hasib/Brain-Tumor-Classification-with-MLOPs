b668612dc853af6d29356388942b6820bc682998
Al-Hasib <mdabdullahalhasib1111@gmail.com>
2024-10-12 00:29:39 +0600

    updated

--- experiment.py
+++ experiment.py
@@ -169,7 +169,7 @@ if __name__ == '__main__':
 
     # Decay LR by a factor of 0.1 every 7 epochs
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-    num_epochs = 10
+    num_epochs = 2
 
     with mlflow.start_run():
         params = {
